742b2bac259b682a4f1cb403a7d8ac7fef73adf3
tellgeniewish <jadastras2@gmail.com>
2024-09-30 23:04:41 +0900

    add review product img

--- js/reviewPage.js
+++ js/reviewPage.js
@@ -2,29 +2,32 @@ const reviews = [
     {
         productId: 'gobdoritang',
         nickname: 'a',
+        profile: "../img/gobdoritang.png",
         date: '2024-09-25',
         rating: 4,
         text: '진짜 너무 너무 맛있어요! 제 식사 스콘은 모네랑 바스크입니다! 색깔이랑 치즈가 최애예요! 다음에는 치즈맛 구매하려고요! 기회되시면 비트맛도 나오면 좋을 것 같아요!',
         product: '맛있딜리, 색색브리 (7개)',
-        image: "../img/gobdoritang.png"
+        reviewImg: "../img/gobdoritang.png"
     },
     {
         productId: 'gobdoritang',
         nickname: 'b',
+        profile: "../img/gobdoritang.png",
         date: '2024-09-24',
         rating: 5,
         text: '좋은 맛이었어요! 하지만 조금 비쌌어요.',
         product: '맛있딜리, 초코브리 (5개)',
-        image: '../img/gobdoritang.png'
+        reviewImg: "../img/gobdoritang.png"
     },
     {
         productId: 'gobdoritang',
         nickname: 'c',
+        profile: "../img/gobdoritang.png",
         date: '2024-09-23',
         rating: 3,
         text: '괜찮은 맛, 하지만 기대에 미치지 못했어요.',
         product: '맛있딜리, 과일브리 (6개)',
-        image: '../img/gobdoritang.png'
+        reviewImg: "../img/gobdoritang.png"
     }
 ];
 
@@ -35,13 +38,14 @@ function displayReviews(sortedReviews) {
     sortedReviews.forEach(review => {
         const reviewHtml = `
             <div class="review_header">
-                <img src="${review.image}" alt="상품 이미지" class="product_image">
+                <img src="${review.profile}" alt="프로필 이미지" class="profile">
                 <div>${review.nickname}</div>
             </div>
             <span class="rating">${'⭐️'.repeat(review.rating)}</span>
             <span>${review.date}</span>
             <div class="review_text">
                 <p>${review.text}</p>
+                <img src="${review.reviewImg}" alt="상품 이미지" class="product_image">
             </div>
             <div class="footer">
                 <p>구매상품: ${review.product}</p>
