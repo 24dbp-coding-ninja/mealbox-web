1a73289c7f8004364b038c0266673f2945dc450f
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2024-10-09 00:01:44 +0900

    장바구니 화면 띄우기

--- mealbox/src/main/java/model/service/CartManager.java
+++ mealbox/src/main/java/model/service/CartManager.java
@@ -14,10 +14,10 @@ public class CartManager {
     // 수량 업데이트 메서드
     public static void updateCartItemQuantity(Cart cart, String productName, int newQuantity) {
         for (CartItem item : cart.getCartItems()) {
-            //if (item.getProduct().getName().equals(productName)) {
+            if (item.getCartItem().getName().equals(productName)) {
                 item.setQuantity(newQuantity);  // 수량 업데이트
                 break;
-           // }
+            }
         }
     }
 
@@ -26,10 +26,10 @@ public class CartManager {
         Iterator<CartItem> iterator = cart.getCartItems().iterator();
         while (iterator.hasNext()) {
             CartItem item = iterator.next();
-            //if (item.getProduct().getName().equals(productName)) {
+            if (item.getCartItem().getName().equals(productName)) {
                 iterator.remove();  // 해당 항목 삭제
                 break;
-            //}
+            }
         }
     }
 }
--- mealbox/src/main/webapp/index.jsp
+++ mealbox/src/main/webapp/index.jsp
@@ -1,1 +1,2 @@
 <% response.sendRedirect(request.getContextPath() + "/user/login/form"); %>
+<!--  response.sendRedirect(request.getContextPath() + "/purchase/cart");  -->
