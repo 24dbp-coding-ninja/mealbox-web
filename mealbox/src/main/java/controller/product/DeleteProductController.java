a146fcf31bc218659ba360e90ca03578f03430ca
tl1l1l1s <shj721553@gmail.com>
2024-12-02 23:58:04 +0900

    [feat] image upload

--- mealbox/src/main/java/controller/product/DeleteProductController.java
+++ mealbox/src/main/java/controller/product/DeleteProductController.java
@@ -4,28 +4,44 @@ import java.util.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import controller.Controller;
 import model.service.ProductManager;
 
 public class DeleteProductController implements Controller {
 
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response)	{
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception	{
 		ProductManager manager = ProductManager.getInstance();        
 
         Map<String, String[]> paramMap = request.getParameterMap();
         List<String> selectedIds = new ArrayList<>();
-        
-        for (String paramName : paramMap.keySet()) {
-            if (paramName.startsWith("checked_")) {
-                selectedIds.add(paramName.replace("checked_", ""));
+
+    	boolean check = ServletFileUpload.isMultipartContent(request);
+    	if(check) {
+    		DiskFileItemFactory factory = new DiskFileItemFactory();
+            ServletFileUpload upload = new ServletFileUpload(factory);
+            
+            List<FileItem> items = upload.parseRequest(request);
+            
+            for (FileItem item : items) {
+                if (item.isFormField()) {
+                    String fieldName = item.getFieldName();
+                    if (fieldName.startsWith("checked_")) {
+                        selectedIds.add(fieldName.replace("checked_", ""));
+                    }
+                }
             }
-        }
-        if (selectedIds == null || selectedIds.size() < 1) {
+    	}
+
+        if (selectedIds.isEmpty()) {
             request.setAttribute("errorMsg", "삭제할 상품을 선택해주세요.");
             return "redirect:/admin";
         }
-		
+        
         int successCount = 0;
         for (String id : selectedIds) {
             successCount += manager.removeProduct(id);
@@ -34,6 +50,7 @@ public class DeleteProductController implements Controller {
         if (successCount != selectedIds.size()) {
             request.setAttribute("errorMsg", "일부 상품 삭제에 실패했습니다.");
         }
+        
         return "redirect:/admin";
     }
 }
